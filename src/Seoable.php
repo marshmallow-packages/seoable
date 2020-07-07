991d7dc9fd06fee6211bb902bcb93621de050a89
Stef van Esch <stef@marshmallow.dev>
2020-07-07 15:20:32 +0200

    Added countable fields to default SEO

--- src/Seoable.php
+++ src/Seoable.php
@@ -3,15 +3,15 @@
 namespace Marshmallow\Seoable;
 
 use Laravel\Nova\Panel;
-use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Image;
 use Laravel\Nova\Fields\Select;
-use Laravel\Nova\Fields\Textarea;
 use Marshmallow\Seoable\Fields\Tags;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Marshmallow\CharcountedFields\TextCounted;
+use Marshmallow\CharcountedFields\TextareaCounted;
 use Marshmallow\Seoable\Models\Route as RouteModel;
 
 class Seoable
@@ -21,60 +21,98 @@ class Seoable
 		'seoable_description',
 	];
 
-	public static function make ($title)
+	public static function make($title)
 	{
 		return new Panel($title, [
 
-            Text::make('Title', 'seoable_title')->fillUsing(function (NovaRequest $request, Model $model, $field) {
-            	/**
-            	 * Only call the store method on the title.
-            	 * This method will store all the available fields.
-            	 */
-                app('seo')->set($model)->store($request, $field, 'title');
+            TextCounted::make('Title', 'seoable_title')
+            	->fillUsing(
+            		function (NovaRequest $request, Model $model, $field) {
+		            	/**
+		            	 * Only call the store method on the title.
+		            	 * This method will store all the available fields.
+		            	 */
+		                app('seo')->set($model)->store($request, $field, 'title');
+	            	}
+           		)
+            	->resolveUsing(
+            		function ($name, Model $model) {
+		            	return app('seo')->set($model)->getSeoTitle();
+		            }
+		        )
+	            ->hideFromIndex()
+	            ->minChars(30)
+			    ->maxChars(60)
+			    ->warningAt(50)
+	            ->hideWhenCreating(),
 
-            })->resolveUsing(function ($name, Model $model) {
-            	return app('seo')->set($model)->getSeoTitle();
-            })->hideFromIndex()->hideWhenCreating(),
 
+            TextareaCounted::make('Description', 'seoable_description')
+            	->fillUsing(
+            		function (NovaRequest $request, Model $model, $field) {
 
-            Textarea::make('Description', 'seoable_description')->fillUsing(function (NovaRequest $request, Model $model, $field) {
-
-                /**
-            	 * Only call the store method on the title.
-            	 * This method will store all the available fields.
-            	 */
-                app('seo')->set($model)->store($request, $field, 'description');
-
-            })->resolveUsing(function ($name, Model $model) {
-            	return app('seo')->set($model)->getSeoDescription();
-            })->hideFromIndex()->hideWhenCreating(),
+		                /**
+		            	 * Only call the store method on the title.
+		            	 * This method will store all the available fields.
+		            	 */
+		                app('seo')->set($model)->store($request, $field, 'description');
+		            }
+		        )
+		        ->resolveUsing(
+		        	function ($name, Model $model) {
+		            	return app('seo')->set($model)->getSeoDescription();
+		            }
+		        )
+		        ->hideFromIndex()
+		        ->minChars(70)
+			    ->maxChars(160)
+			    ->warningAt(150)
+		        ->hideWhenCreating(),
 
             Tags::make('Tags', 'seoable_tags')->withoutSuggestions()->hideFromIndex()->hideWhenCreating(),
 
-            Select::make('Follow type', 'seoable_follow_type')->options(config('seo.follow_type_options'))->fillUsing(function (NovaRequest $request, Model $model, $field) {
-                /**
-                 * Only call the store method on the title.
-                 * This method will store all the available fields.
-                 */
-                app('seo')->set($model)->store($request, $field, 'follow_type');
+            Select::make('Follow type', 'seoable_follow_type')
+            	->options(config('seo.follow_type_options'))
+            	->fillUsing(
+            		function (NovaRequest $request, Model $model, $field) {
+		                /**
+		                 * Only call the store method on the title.
+		                 * This method will store all the available fields.
+		                 */
+		                app('seo')->set($model)->store($request, $field, 'follow_type');
+		            }
+		        )
+            	->resolveUsing(
+            		function ($name, Model $model) {
+		                return app('seo')->set($model)->getSeoFollowType();
+		            }
+		        )
+		        ->hideFromIndex()
+		        ->hideWhenCreating(),
 
-            })->resolveUsing(function ($name, Model $model) {
-                return app('seo')->set($model)->getSeoFollowType();
-            })->hideFromIndex()->hideWhenCreating(),
-
-            Image::make('Image', 'seoable_image')->store(function (NovaRequest $request, Model $model, $attribute, $requestAttribute, $disk, $storagePath) {
-
-                $storage_location = Storage::disk(config('seo.storage.disk'))->putFile(config('seo.storage.path'), $request->file($requestAttribute));
-                $request->{$requestAttribute} = $storage_location;
-                app('seo')->set($model)->store($request, $requestAttribute, 'image');
-
-            })->preview(function ($value, $disk, Model $model) {
-                return app('seo')->set($model)->getSeoImageUrl();
-            })->hideFromIndex()->hideWhenCreating(),
+            Image::make('Image', 'seoable_image')
+            	->store(
+            		function (NovaRequest $request, Model $model, $attribute, $requestAttribute, $disk, $storagePath) {
+		                $storage_location = Storage::disk(config('seo.storage.disk'))->putFile(config('seo.storage.path'), $request->file($requestAttribute));
+		                $request->{$requestAttribute} = $storage_location;
+		                app('seo')
+		                	->set($model)
+		                	->store($request, $requestAttribute, 'image');
+		            }
+		        )
+            	->preview(
+            		function ($value, $disk, Model $model) {
+		                return app('seo')
+	                			->set($model)
+	                			->getSeoImageUrl();
+		            }
+		        )
+		        ->hideFromIndex()
+		        ->hideWhenCreating(),
         ]);
 	}
 
-    public static function routes ()
+    public static function routes()
     {
         $routes = RouteModel::ordered()->get();
         foreach ($routes as $route) {
