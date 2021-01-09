dfb0c283a8979d5e338e9a97ef625c438c1938e5
Stef van Esch <stef@marshmallow.dev>
2021-01-09 19:10:49 +0100

    How To Step Schema

---
+++ src/Helpers/Schemas/SchemaHowToStep.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Marshmallow\Seoable\Helpers\Schemas;
+
+use Marshmallow\Seoable\Helpers\Schemas\Traits\Makeable;
+
+class SchemaHowToStep extends Schema
+{
+    use Makeable;
+
+    protected $text;
+
+    protected $url;
+
+    protected $image;
+
+    public function image(string $image)
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    public function toJson()
+    {
+        return [
+            '@type' => 'HowToStep',
+            'name' => $this->name,
+            'text' => $this->text,
+            'url' => $this->url,
+            'image' => $this->image,
+        ];
+    }
+}
