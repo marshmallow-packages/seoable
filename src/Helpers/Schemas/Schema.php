57eb4d5fe806f3d7207e96ae78cf67cb179769ef
Stef van Esch <stef@marshmallow.dev>
2021-01-09 19:11:38 +0100

    Added new schemas

--- src/Helpers/Schemas/Schema.php
+++ src/Helpers/Schemas/Schema.php
@@ -3,20 +3,39 @@
 namespace Marshmallow\Seoable\Helpers\Schemas;
 
 use Carbon\Carbon;
+use Marshmallow\Seoable\Helpers\Schemas\SchemaInteractionCounter;
 
 class Schema
 {
+    protected $brand;
+
+    protected $rating;
+
+    protected $aggregateRating;
+
+    protected $interactionStatistic;
+
+    protected $datePublished;
+
+    protected $description;
+
+    protected $video;
+
+    protected $offers = [];
+
+    protected $images = [];
+
+    protected $reviews = [];
+
     public function brand(string $name)
     {
         $this->brand = SchemaBrand::make($name)->toJson();
-
         return $this;
     }
 
     public function offer(SchemaOffer $offer)
     {
         $this->offers([$offer]);
-
         return $this;
     }
 
@@ -28,14 +47,12 @@ class Schema
             }
             $this->offers[] = $offer->toJson();
         }
-
         return $this;
     }
 
     public function rating(SchemaRating $rating)
     {
         $this->rating = $rating;
-
         return $this;
     }
 
@@ -90,11 +107,59 @@ class Schema
         return $this;
     }
 
+    public function video(SchemaVideoObject $video)
+    {
+        $this->video = $video;
+        return $this;
+    }
+
+    public function interactionStatistic(SchemaInteractionCounter $counter)
+    {
+        $this->interactionStatistic = $counter;
+        return $this;
+    }
+
+    protected function getDurationStringFromSeconds(string $column, int $seconds)
+    {
+        $time = gmdate("H:i:s", $seconds);
+        $parts = collect(explode(':', $time))->map(function ($part) {
+            return intval($part);
+        })->toArray();
+
+        $duration = 'PT';
+        $duration .= $parts[0] . 'H';
+        $duration .= $parts[1] . 'M';
+        $duration .= $parts[2] . 'S';
+
+        $this->{$column} = $duration;
+        return $this;
+    }
+
+    protected function getJsonSchema($column)
+    {
+        if (! $this->{$column}) {
+            return null;
+        }
+        $data = $this->{$column}->toJson();
+        return array_filter($data);
+    }
+
     public function toJson()
     {
         $array = $this->toArray();
         $array = array_filter($array);
 
         return $array;
+    }
+
+    public function __call($parameter, $arguments)
+    {
+        $this->{$parameter} = $arguments[0];
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return json_encode($this->toJson());
     }
 }
