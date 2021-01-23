e78c01c1ad106dd8b014cc5ae1957f36017be78e
Stef van Esch <stef@marshmallow.dev>
2021-01-23 15:56:30 +0100

    Hide MR mallow

--- resources/views/seo_body.blade.php
+++ resources/views/seo_body.blade.php
@@ -1,3 +1,9 @@
+@if (!config('seo.hide_mr_mallow'))
 <!-- Marshmallow SEO -->
+@endif
+
 @include('seoable::google.gtm-body')
+
+@if (!config('seo.hide_mr_mallow'))
 <!-- Marshmallow SEO -->
+@endif
