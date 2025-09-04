fc45715682f30d627f232a6ef25935ddb7a6e257
LTKort <lars.kort94@gmail.com>
2025-09-04 13:09:02 +0200

    (fix) Only twitter card if Site is present

--- resources/views/seo.blade.php
+++ resources/views/seo.blade.php
@@ -17,21 +17,32 @@
     <meta name="robots" content="{{ Seo::getSeoFollowType() }}" />
 @endif
 <meta name="author" content="{{ config('seo.defaults.author') }}">
-<meta name="twitter:card" content="{{ config('seo.twitter.card') }}" />
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 @if (config('seo.fields.title'))
     <meta property="og:title" content="{{ Seo::getSeoTitle() }}" />
-    <meta property="twitter:title" content="{{ Seo::getSeoTitle() }}" />
 @endif
 @if (config('seo.fields.description'))
     <meta property="og:description" content="{{ Seo::getSeoDescription() }}" />
-    <meta property="twitter:description" content="{{ Seo::getSeoDescription() }}" />
 @endif
 @if (config('seo.fields.image'))
     <meta property="og:image" content="{{ Seo::getSeoImageUrl() }}" />
-    <meta property="twitter:image" content="{{ Seo::getSeoImageUrl() }}" />
 @endif
+
+@if (config('seo.twitter.site'))
+    <meta name="twitter:card" content="{{ config('seo.twitter.card') }}" />
+    <meta name="twitter:site" content="{{ config('seo.twitter.site') }}" />
+    @if (config('seo.fields.title'))
+        <meta property="twitter:title" content="{{ Seo::getSeoTitle() }}" />
+    @endif
+    @if (config('seo.fields.description'))
+        <meta property="twitter:description" content="{{ Seo::getSeoDescription() }}" />
+    @endif
+    @if (config('seo.fields.image'))
+        <meta property="twitter:image" content="{{ Seo::getSeoImageUrl() }}" />
+    @endif
+@endif
+
 <meta property="og:url" content="{{ Seo::getSeoCanonicalUrl() }}" />
 <meta property="og:site_name" content="{{ config('seo.defaults.sitename') }}" />
 <meta property="og:locale" content="{{ Seo::getSeoLocale() }}" />
@@ -50,9 +61,6 @@
     <meta property="og:type" content="{{ $type }}" />
 @endif
 
-@if (config('seo.twitter.site'))
-    <meta name="twitter:site" content="{{ config('seo.twitter.site') }}" />
-@endif
 @if (config('seo.twitter.creator'))
     <meta name="twitter:creator" content="{{ config('seo.twitter.creator') }}" />
 @endif
